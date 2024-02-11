d045b9692c0f99b295c14c8587788e241380388d
darya-yarosh <gurimusagi@yandex.ru>
2024-02-11 23:49:05 +0300

    Created server main file.

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const { isInvalidFields, getInvalidFields } = require("./helper.js");
+
+const app = express();
+
+const port = 9090;
+
+app.use(cors());
+const jsonParser = bodyParser.json();
+
+app.post("/api/registration", (req, res) => {
+    if (Math.random() > 0.5) {
+        res.statusCode = 400;
+
+        setTimeout(() => {
+            res.send({
+                status: "error",
+                message: "Bad request",
+            });
+        }, Math.random() * 1000);
+
+        return;
+    }
+
+    setTimeout(() => {
+        res.statusCode = 200;
+        res.send({
+            status: "success",
+            message: "You are registered",
+        });
+    }, Math.random() * 1000);
+});
+
+app.get("/api/ping", (req, res) => {
+    res.statusCode = 200;
+    res.send({
+        status: "success",
+        message: "Server is ready",
+    });
+});
+
+app.post("/api/feedback", jsonParser, (req, res) => {
+    const fields = getInvalidFields(req.body)
+
+    if (isInvalidFields(fields)) {
+        res.send({
+            status: "error",
+            fields: fields,
+        })
+    } else {
+        res.send({
+            status: "success",
+            msg: "Ваша заявка успешно отправлена",
+        })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
